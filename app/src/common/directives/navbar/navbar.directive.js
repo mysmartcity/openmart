b2e337271203bba02b5d177433a66632afc127be
petru <petru.isfan@gmail.com>
2015-04-19 13:00:07 +0300

    Underline for navbar

--- app/src/common/directives/navbar/navbar.directive.js
+++ app/src/common/directives/navbar/navbar.directive.js
@@ -3,10 +3,15 @@
     'use strict';
 
     angular.module("directives.navbar", [])
-        .directive("navbar", [function() {
+        .directive("navbar", ["$location", function($location) {
             return {
                 restrict: "AE",
-                templateUrl: "src/common/directives/navbar/navbar.html"
+                templateUrl: "src/common/directives/navbar/navbar.html",
+                link: function($scope) {
+                    $scope.atPage = function(page) {
+                        return $location.path() === page;
+                    }
+                }
             }
         }]);
 } ());
